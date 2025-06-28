31172031a6cd28e572812f9d24557002342411ee
Anton-013 <antonchuvashovoren@mail.ru>
2025-06-28 18:56:29 +0300

    refactoring Tasks & TaskItem

---
+++ src/TaskItem.tsx
@@ -0,0 +1,45 @@
+import ListItem from "@mui/material/ListItem"
+import { useAppDispatch } from "./common/hooks/useAppDispatch"
+import { changeTaskStatusAC, changeTaskTitleAC, deleteTaskAC } from "./model/tasks-reducer"
+import { getListItemSx } from "./TodolistItem.styles"
+import Checkbox from "@mui/material/Checkbox"
+import { EditableSpan } from "./EditableSpan"
+import IconButton from "@mui/material/IconButton"
+import DeleteIcon from '@mui/icons-material/Delete'
+import { ChangeEvent } from "react"
+import { Task } from "./app/App"
+
+type Props = {
+    task: Task
+    todolistId: string
+}
+
+export const TaskItem = ({task, todolistId}: Props) => {
+
+    const dispatch = useAppDispatch()
+
+    const deleteTaskHandler = () => {
+        dispatch(deleteTaskAC({ todolistId, taskId: task.id }))
+    }
+
+    const changeTaskStatusHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        const newStatusValue = e.currentTarget.checked
+        dispatch(changeTaskStatusAC({ todolistId, taskId: task.id, isDone: newStatusValue }))
+    }
+
+    const changeTaskTitleHandler = (title: string) => {
+        dispatch(changeTaskTitleAC({ todolistId, taskId: task.id, title }))
+    }
+
+    return (
+        <ListItem sx={getListItemSx(task.isDone)}>
+            <div>
+                <Checkbox checked={task.isDone} onChange={changeTaskStatusHandler} />
+                <EditableSpan value={task.title} onChange={changeTaskTitleHandler} />
+            </div>
+            <IconButton onClick={deleteTaskHandler}>
+                <DeleteIcon />
+            </IconButton>
+        </ListItem>
+    )
+}
