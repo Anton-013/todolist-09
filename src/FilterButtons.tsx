4e83c76d2e0e53b99ffb0c411d3ccfce4b4737d1
Anton-013 <antonchuvashovoren@mail.ru>
2025-06-28 18:37:29 +0300

    refactoring FilterButton

---
+++ src/FilterButtons.tsx
@@ -0,0 +1,40 @@
+import Box from "@mui/material/Box"
+import Button from '@mui/material/Button'
+import { useAppDispatch } from "./common/hooks/useAppDispatch"
+import { changeTodolistFilterAC } from "./model/todolists-reducer"
+import { containerSx } from "./TodolistItem.styles"
+import { FilterValues, Todolist } from "./app/App"
+
+type Props = {
+  todolist: Todolist
+}
+
+export const FilterButtons = ({ todolist }: Props) => {
+    const { id, filter } = todolist
+
+    const dispatch = useAppDispatch() 
+
+    const changeFilterHandler = (filter: FilterValues) => {
+        dispatch(changeTodolistFilterAC({ id, filter }))
+    }
+
+    return (
+        <Box sx={containerSx}>
+            <Button variant={filter === 'all' ? 'outlined' : 'text'}
+                color={'inherit'}
+                onClick={() => changeFilterHandler('all')}>
+                All
+            </Button>
+            <Button variant={filter === 'active' ? 'outlined' : 'text'}
+                color={'primary'}
+                onClick={() => changeFilterHandler('active')}>
+                Active
+            </Button>
+            <Button variant={filter === 'completed' ? 'outlined' : 'text'}
+                color={'secondary'}
+                onClick={() => changeFilterHandler('completed')}>
+                Completed
+            </Button>
+        </Box>
+    )
+}
